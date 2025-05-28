50847591ff2b9cfe9fb7dc405ac6d9b1a62cfc0f
Sweta <Shweta@auxgens.net>
2025-05-28 11:39:54 +0530

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://nevaloss-portfolio.com/dorman_app/';
+$config['base_url'] = 'dormanapp-btd8apa0ewgyagha.canadacentral-01.azurewebsites.net';
 
 /*
 |--------------------------------------------------------------------------
